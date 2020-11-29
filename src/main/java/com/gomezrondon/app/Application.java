be4b80e34fd486b29e7fc365976dc50e260d87a6
gomezrondon <gomezrondon@gmail.com>
2020-11-29 10:28:09 -0600

    1rst Commit.

---
+++ src/main/java/com/gomezrondon/app/Application.java
@@ -0,0 +1,23 @@
+package com.gomezrondon.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
+	@GetMapping("/time")
+	public String getTime() {
+		return LocalDateTime.now().toString();
+	}
+
+}
